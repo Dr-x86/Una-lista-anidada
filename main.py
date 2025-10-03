fd1d3de76061c59bd36dc035a8a8a49b0ee44adc
Dr-x86 <romeroibarradaniel@gmail.com>
2025-10-02 20:49:14 -0600

    Version Actions

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ TOKEN = os.getenv("TOKEN")
 """
 
 
-lista_subs = ["hentai","masteruwuoficial","spicyteto","HatsuneMiku_Hentai","hatsunemikuhentaiv3","vocaloidhentai","KasaneTetoRule34"]
+lista_subs = ["hentai","masteruwuoficial","spicyteto","HatsuneMiku_Hentai","hatsunemikuhentaiv3","vocaloidhentai"]
 
 def registrar_url(url):
     with open("urls.txt", 'a') as f:
